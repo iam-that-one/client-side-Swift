2f89d3a01c4ddbadaef07d6d5bec9b964b01ed08
Abdullah Alnutayfi <ccsi-iuni@hotmail.com>
2022-08-07 13:12:25 +0300

    Initial Commit

---
+++ Front-end/Front_endApp.swift
@@ -0,0 +1,17 @@
+//
+//  Front_endApp.swift
+//  Front-end
+//
+//  Created by Abdullah Alnutayfi on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Front_endApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
